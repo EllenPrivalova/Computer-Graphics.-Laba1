a3192831d2f872f8921a0b062aa7583ced80b69d
Helen Privalova <privalova.elena.200@mail.ru>
2024-09-23 23:57:45 +0300

    КВ.Д.Г2

--- main.cpp
+++ main.cpp
@@ -11,174 +11,124 @@
 using namespace sf;
 using namespace std;
 
-// Утилита для проекции точки на ось
-float dotProduct(const Vector2f& a, const Vector2f& b) {
-    return a.x * b.x + a.y * b.y;
-}
-
-// Получаем нормализованную ось для SAT (нормаль к стороне)
-Vector2f getNormal(Vector2f& edge) {
-    return Vector2f(-edge.y, edge.x);
-}
-
-
 class AdvancedTriangle {
 private:
-    ConvexShape triangle;
-    Vector2f velocity;
-    Color fillColor;
-    Color outlineColor;
-    float mass;
+	ConvexShape triangle;
+	Vector2f velocity;
+	Color fillColor;
+	Color outlineColor;
+	float speed;
 
 public:
-    AdvancedTriangle(float side, Vector2f initialPosition, Vector2f initialVelocity, float mass) {
-        // Задаем форму треугольника
-        triangle.setPointCount(3);
-        triangle.setPoint(0, Vector2f(0, 0));
-        triangle.setPoint(1, Vector2f(side, 0));
-        triangle.setPoint(2, Vector2f(side / 2, -side * sqrt(3) / 2));
+	AdvancedTriangle(float side, Vector2f initialPosition, Vector2f initialVelocity) {
+		// Задаем форму треугольника
+		triangle.setPointCount(3);
+		triangle.setPoint(0, Vector2f(0, 0));
+		triangle.setPoint(1, Vector2f(side, 0));
+		triangle.setPoint(2, Vector2f(side / 2, -side * sqrt(3) / 2));
 
-        triangle.setPosition(initialPosition);
-        velocity = initialVelocity;
+		triangle.setPosition(initialPosition);
+		velocity = initialVelocity;
 
-        this->mass = mass;
+		fillColor = Color(40, 40, 40);
+		outlineColor = Color::Blue;
 
-        fillColor = Color(40, 40, 40);
-        outlineColor = Color::Blue;
+		triangle.setFillColor(fillColor);
+		triangle.setOutlineColor(outlineColor);
+		triangle.setOutlineThickness(1);
+	}
 
-        triangle.setFillColor(fillColor);
-        triangle.setOutlineColor(outlineColor);
-        triangle.setOutlineThickness(1);
-    }
+	void updateVelocity(float multiplier) {
+		velocity *= multiplier;
+	}
 
-    void projectOntoAxis(const Vector2f& axis, float& min, float& max) {
-        min = max = dotProduct(triangle.getTransform().transformPoint(triangle.getPoint(0)), axis);
-        for (int i = 1; i < 3; ++i) {
-            float projection = dotProduct(triangle.getTransform().transformPoint(triangle.getPoint(i)), axis);
-            if (projection < min) {
-                min = projection;
-            }
-            if (projection > max) {
-                max = projection;
-            }
-        }
-    }
+	void updatePosition(RenderWindow& window) {
+		// Обновление позиции
+		Vector2f position = triangle.getPosition();
+		position += velocity;
 
-    bool checkCollision(AdvancedTriangle& other) {
-        vector<Vector2f> axesToTest;
+		// Проверка столкновения с границами экрана
+		if (position.x <= 0 || position.x + triangle.getPoint(1).x >= window.getSize().x) {
+			velocity.x = -velocity.x;
+			changeColor(fillColor);
+		}
 
-        // Получаем стороны треугольников для тестирования осей
-        for (int i = 0; i < 3; ++i) {
-            Vector2f edge1 = triangle.getPoint((i + 1) % 3) - triangle.getPoint(i);
-            Vector2f edge2 = other.triangle.getPoint((i + 1) % 3) - other.triangle.getPoint(i);
-            axesToTest.push_back(getNormal(edge1));
-            axesToTest.push_back(getNormal(edge2));
-        }
+		if (position.y + triangle.getPoint(2).y <= 0 || position.y >= window.getSize().y) {
+			velocity.y = -velocity.y;
+			changeColor(fillColor);
+		}
 
-        for (auto& axis : axesToTest) {
-            float minA, maxA, minB, maxB;
-            projectOntoAxis(axis, minA, maxA);
-            other.projectOntoAxis(axis, minB, maxB);
+		triangle.setPosition(position);
+	}
 
-            // Если на какой-то оси проекции не пересекаются, столкновения нет
-            if (maxA < minB || maxB < minA) {
-                return false;
-            }
-        }
-        return true; // Столкновение произошло
-    }
+	void changeColor(Color& color) {
+		fillColor = Color(rand() % 256, rand() % 256, rand() % 256);
+		triangle.setFillColor(fillColor);
+	}
 
-    void resolveCollision(AdvancedTriangle& other) {
-        Vector2f newVelocity1 = velocity;
-        Vector2f newVelocity2 = other.velocity;
+	void draw(RenderWindow& window) {
+		window.draw(triangle);
+	}
 
-        float totalMass = mass + other.mass;
-        newVelocity1 = (velocity * (mass - other.mass) + other.velocity * (2 * other.mass)) / totalMass;
-        newVelocity2 = (other.velocity * (other.mass - mass) + velocity * (2 * mass)) / totalMass;
+	void erase(RenderWindow& window, Color& backgroundColor) {
+		triangle.setOutlineThickness(10);
+		triangle.setFillColor(backgroundColor);
+		triangle.setOutlineColor(backgroundColor);
 
-        // Обновляем скорости после столкновения
-        velocity = newVelocity1;
-        other.velocity = newVelocity2;
-    }
+		draw(window);
 
-    void updatePosition(RenderWindow& window) {
-        // Обновление позиции
-        Vector2f position = triangle.getPosition();
-        position += velocity;
-
-        // Проверка столкновения с границами экрана
-        if (position.x <= 0 || position.x + triangle.getPoint(1).x >= window.getSize().x) {
-            velocity.x = -velocity.x;
-            changeColor(fillColor);
-        }
-
-        if (position.y + triangle.getPoint(2).y <= 0 || position.y >= window.getSize().y) {
-            velocity.y = -velocity.y;
-            changeColor(fillColor);
-        }
-
-        triangle.setPosition(position);
-    }
-
-    void changeColor(Color& color) {
-        fillColor = Color(rand() % 256, rand() % 256, rand() % 256);
-        triangle.setFillColor(fillColor);
-    }
-
-    void draw(RenderWindow& window) {
-        window.draw(triangle);
-    }
-
-    void erase(RenderWindow& window, Color& backgroundColor) {
-        triangle.setOutlineThickness(3);
-        triangle.setFillColor(backgroundColor);
-        triangle.setOutlineColor(backgroundColor);
-
-        draw(window);
-
-        triangle.setOutlineThickness(1);
-        triangle.setFillColor(fillColor);
-        triangle.setOutlineColor(outlineColor);
-    }
+		triangle.setOutlineThickness(1);
+		triangle.setFillColor(fillColor);
+		triangle.setOutlineColor(outlineColor);
+	}
 };
 
 int main() {
-    // Создание окна
-    RenderWindow window(VideoMode(800, 600), "Laba1");
+	// Создание окна
+	RenderWindow window(VideoMode(800, 600), "Laba1");
 
-    AdvancedTriangle advancedTriangle1(100, Vector2f(400, 300), Vector2f(1.0f, 1.5f), 1.0f);
-    AdvancedTriangle advancedTriangle2(100, Vector2f(500, 400), Vector2f(-1.5f, -1.f), 1.0f);
+	AdvancedTriangle advancedTriangle(100, Vector2f(400, 300), Vector2f(-1.f, 1.f));
 
-    Color backgroundColor = Color::Black;
+	Color backgroundColor = Color::Black;
 
-    Clock clock;
-    float t = 1.f / FRAMERATE;
+	Clock clock;
+	float t = 1.f / FRAMERATE;
 
-    while (window.isOpen()) {
-        Event event;
-        while (window.pollEvent(event)) {
-            if (event.type == Event::Closed)
-                window.close();
-        }
+	float velocityMultiple = 1;
+	int currentGear = 2;
 
-        advancedTriangle1.erase(window, backgroundColor);
-        advancedTriangle2.erase(window, backgroundColor);
+	while (window.isOpen()) {
+		Event event;
+		while (window.pollEvent(event)) {
+			if (event.type == Event::KeyPressed && event.key.code == Keyboard::Up) {
+				if ((currentGear + 1) < 5) {
+					advancedTriangle.updateVelocity(2);
+					currentGear++;
+				}
+			}
 
-        advancedTriangle1.updatePosition(window);
-        advancedTriangle2.updatePosition(window);
+			if (event.type == Event::KeyPressed && event.key.code == Keyboard::Down) {
+				if (currentGear - 1 > 0) {
+					advancedTriangle.updateVelocity(0.5); 
+					currentGear--;
+				}
+			}
 
-        if (advancedTriangle1.checkCollision(advancedTriangle2)) {
-            advancedTriangle1.resolveCollision(advancedTriangle2);
-        }
+			if (event.type == Event::Closed)
+				window.close();
+		}
 
-        advancedTriangle1.draw(window);
-        advancedTriangle2.draw(window);
+		advancedTriangle.erase(window, backgroundColor);
 
-        window.display();
+		advancedTriangle.updatePosition(window);
 
-        auto elapsed = clock.restart();
-        sleep(seconds(t - elapsed.asSeconds()));
-    }
+		advancedTriangle.draw(window);
 
-    return 0;
+		window.display();
+
+		auto elapsed = clock.restart();
+		sleep(seconds(t - elapsed.asSeconds()));
+	}
+
+	return 0;
 }
